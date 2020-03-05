eefd2d8d68a484bb56f0e5bc3c91c912cf3e3749
u230925 <u230925@gmail.com>
2020-03-05 06:08:35 +0300

    AC

--- driver-avalon8.c
+++ driver-avalon8.c
@@ -208,7 +208,7 @@ struct avalon8_dev_description avalon8_dev_table_lp[] = {
 			AVA8_DEFAULT_FREQUENCY_0M,
 			AVA8_DEFAULT_FREQUENCY_0M,
 			AVA8_DEFAULT_FREQUENCY_0M,
-			AVA8_DEFAULT_FREQUENCY_500M
+			AVA8_DEFAULT_FREQUENCY_10M
 		}
 	},
 	{
